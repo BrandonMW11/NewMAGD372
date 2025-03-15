792e556b6d8ef8a556b3b4cca6b6ddfe07e460f2
BrandonMW11 <brandonwaltersdorf@gmail.com>
2025-03-15 14:20:05 -0500

    Terrain Generator
    It's not actually terrain though

--- MAGD372/Assets/MeshGenerator.cs
+++ MAGD372/Assets/MeshGenerator.cs
@@ -10,6 +10,10 @@ public class MeshGenerator : MonoBehaviour
     Vector3[] vertices;
     int[] triangles;
 
+    //Vertex count = (xSize + 1) * (zSize + 1)
+    public int xSize = 20;
+    public int zSize = 20;
+
     void Start()
     {
         mesh = new Mesh();
@@ -21,7 +25,8 @@ public class MeshGenerator : MonoBehaviour
 
     void CreateShape() 
     {
-        vertices = new Vector3[] 
+        //Single Triangle
+        /*vertices = new Vector3[] 
         {
             new Vector3(0,0,0),
             new Vector3(0,0,1),
@@ -33,7 +38,41 @@ public class MeshGenerator : MonoBehaviour
         {
             0, 1, 2,
             1, 3, 2
-        };
+        };*/
+
+        //Terrain
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for(int i = 0, z = 0; z <= zSize; z++) 
+        {
+            for(int x = 0; x <= xSize; x++) 
+            {
+                float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
+                vertices[i] = new Vector3(x, y, z);
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
     }
 
     void UpdateMesh() 
@@ -44,4 +83,17 @@ public class MeshGenerator : MonoBehaviour
         mesh.triangles = triangles;
         mesh.RecalculateNormals();
     }
+
+    /*private void OnDrawGizmos() 
+    {
+        if(vertices == null) 
+        {
+            return;
+        }
+
+        for(int i = 0; i < vertices.Length; i++) 
+        {
+            Gizmos.DrawSphere(vertices[i], .1f);
+        }
+    }*/
 }
